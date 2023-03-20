501259de61e0b8d2c483c21c669db23c0b95b913
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-20 23:15:37 +0700

    Request Body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -59,7 +60,7 @@ async def list_items(skip: int = 0, limit: int = 10):
 
 @app.get("/items/{item_id}")
 async def get_item(
-    item_id: str, sample_query_param: str, q: str | None = None, short: bool = False
+    item_id: str, sample_query_param: str, q: Optional[str] = None, short: bool = False
 ):
     item = {"item_id": item_id, "sample_query_param": sample_query_param}
     if q:
@@ -87,3 +88,27 @@ async def get_user_item(
             }
         )
         return item
+    
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result

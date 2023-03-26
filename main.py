b3dab6cdfe8f29252a4f798372bdb13fad8863f7
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-26 23:46:20 +0700

    Part 12: Cookie and Header Parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, Field, HttpUrl
@@ -300,21 +300,35 @@ app = FastAPI()
 #     return results
 
 ## Part 11 - Extra Data Types
-@app.put("/items/{item_id}")
-async def read_items(item_id: UUID, 
-                     start_date: datetime | None = Body(None), 
-                     end_date: datetime | None = Body(None),
-                     repeat_at: time | None = Body(None),
-                     process_after: timedelta | None = Body(None)):
-    start_process = start_date + process_after
-    duration = end_date - start_process
-    return {"item_id": item_id, 
-            "start_date": start_date,
-            "end_date": end_date,
-            "repeat_at": repeat_at,
-            "process_after": process_after,
-            "start_process": start_process,
-            "duration": duration}
+# @app.put("/items/{item_id}")
+# async def read_items(item_id: UUID, 
+#                      start_date: datetime | None = Body(None), 
+#                      end_date: datetime | None = Body(None),
+#                      repeat_at: time | None = Body(None),
+#                      process_after: timedelta | None = Body(None)):
+#     start_process = start_date + process_after
+#     duration = end_date - start_process
+#     return {"item_id": item_id, 
+#             "start_date": start_date,
+#             "end_date": end_date,
+#             "repeat_at": repeat_at,
+#             "process_after": process_after,
+#             "start_process": start_process,
+#             "duration": duration}
+
+## Part 12 - Cookie and Header Parameter
+@app.get("/items")
+async def read_items(cookie_id: str | None = Cookie(None),
+                     accept_encoding: str | None = Header(None),
+                     sec_ch_ua: str | None = Header(None),
+                     user_agent: str | None = Header(None),
+                     x_token: list[str] | None = Header(None)
+                     ):
+    return {"cookie_id": cookie_id,
+            "Accept-Encoding": accept_encoding,
+            "sec-ch-ua": sec_ch_ua,
+            "User-Agent": user_agent,
+            "X-Token values": x_token}
 
 
 

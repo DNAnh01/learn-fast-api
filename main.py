e2af65fe9b543917937bc164ea6b39ba9f1710f4
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-26 23:10:30 +0700

    Part 11: Extra Data Types

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Query, Path, Body
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, Field, HttpUrl
+from uuid import UUID # part 11
+from datetime import datetime, time, timedelta
 
 app = FastAPI()
 
@@ -242,59 +244,77 @@ app = FastAPI()
 
 
 ## Part 10 - Declare Request Example Data
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
 
-    # class Config:
-    #     schema_extra = {
-    #         "example": {
-    #             "name": "Foo",
-    #             "description": "A very nice Item",
-    #             "price": 16.25,
-    #             "tax": 1.67,
-    #         }
-    #     }
+#     # class Config:
+#     #     schema_extra = {
+#     #         "example": {
+#     #             "name": "Foo",
+#     #             "description": "A very nice Item",
+#     #             "price": 16.25,
+#     #             "tax": 1.67,
+#     #         }
+#     #     }
 
 
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Item = Body(
+#         ...,
+#         examples={
+#             "normal": {
+#                 "summary": "A normal example",
+#                 "description": "A __normal__ item works _correctly_",
+#                 "value": {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 16.25,
+#                     "tax": 1.67,
+#                 },
+#             },
+#             "converted": {
+#                 "summary": "An example with converted data",
+#                 "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+#                 "value": {
+#                     "name": "Bar",
+#                     "price": "16.25"
+#                 },
+#             },
+#             "invalid": {
+#                 "summary": "Invalid data is rejected with an error",
+#                 "description": "Hello youtubers",
+#                 "value": {
+#                     "name": "Baz", 
+#                     "price": "sixteen point two five"
+#                 },
+#             },
+#         },
+#     ),
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+## Part 11 - Extra Data Types
 @app.put("/items/{item_id}")
-async def update_item(
-    item_id: int,
-    item: Item = Body(
-        ...,
-        examples={
-            "normal": {
-                "summary": "A normal example",
-                "description": "A __normal__ item works _correctly_",
-                "value": {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 16.25,
-                    "tax": 1.67,
-                },
-            },
-            "converted": {
-                "summary": "An example with converted data",
-                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                "value": {
-                    "name": "Bar",
-                    "price": "16.25"
-                },
-            },
-            "invalid": {
-                "summary": "Invalid data is rejected with an error",
-                "description": "Hello youtubers",
-                "value": {
-                    "name": "Baz", 
-                    "price": "sixteen point two five"
-                },
-            },
-        },
-    ),
-):
-    results = {"item_id": item_id, "item": item}
-    return results
+async def read_items(item_id: UUID, 
+                     start_date: datetime | None = Body(None), 
+                     end_date: datetime | None = Body(None),
+                     repeat_at: time | None = Body(None),
+                     process_after: timedelta | None = Body(None)):
+    start_process = start_date + process_after
+    duration = end_date - start_process
+    return {"item_id": item_id, 
+            "start_date": start_date,
+            "end_date": end_date,
+            "repeat_at": repeat_at,
+            "process_after": process_after,
+            "start_process": start_process,
+            "duration": duration}
+
 
 

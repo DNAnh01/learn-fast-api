e7332684e3aa83b914e6f504ef898431966b1ddb
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-28 16:13:27 +0700

    Part 15 - Response Status Codes

--- main.py
+++ main.py
@@ -481,19 +481,26 @@ app = FastAPI()
 # async def get_arbitrary():
 #     return {"foo": 1, "bar": "2"}
 
-## Part 15 - Response Status Codes
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+# ## Part 15 - Response Status Codes
+# @app.post("/items/", status_code=status.HTTP_201_CREATED)
+# async def create_item(name: str):
+#     return {"name": name}
 
 
-@app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_item(pk: str):
-    print("pk", pk)
-    return pk
+# @app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_item(pk: str):
+#     print("pk", pk)
+#     return pk
 
 
-@app.get("/items/", status_code=status.HTTP_302_FOUND)
-async def read_items_redirect():
-    return {"hello": "world"}
+# @app.get("/items/", status_code=status.HTTP_302_FOUND)
+# async def read_items_redirect():
+#     return {"hello": "world"}
+
+## Part 16 - Form Fields
+
+
+
+
+
 
